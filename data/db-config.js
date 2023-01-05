b00afe405469acd029a0ded0df9db5a840ad41e6
clairelist <claire.list2501@gmail.com>
2023-01-04 19:03:39 -0800

    recreating database using knex migrations. working...

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
-const { development } = require('../../knexfile');
-const knexfile = require('../../knexfile');
+const { development } = require('../knexfile');
+const knexfile = require('../knexfile');
 
 //const env = process.env.NODE_ENV || 'development';
 //USED FOR PRODUCTION! for now, call knexfile[development], below.
---
+++ data/migrations/20230105025523_users.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('users', users =>{
+        users.increments('user_id')
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
