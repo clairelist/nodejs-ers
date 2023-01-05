127455439bc100008661deaeeadc1768bc151d6f
clairelist <claire.list2501@gmail.com>
2023-01-04 19:38:34 -0800

    basic migration:up!

--- data/migrations/20230105025523_users.js
+++ data/migrations/20230105025523_users.js
@@ -5,7 +5,20 @@
 exports.up = function(knex) {
   return knex.schema
     .createTable('users', users =>{
-        users.increments('user_id')
+        users.increments('user_id');
+        users.string('email', 32).notNullable().unique();
+        users.string('password', 64).notNullable();
+        users.string('first_name', 32).notNullable();
+        users.string('last_name', 32).notNullable();
+        users.string('phone_number', 16);
+        users.string('role', 16).notNullable().defaultTo('EMPLOYEE');
+
+    })
+    .createTable('tickets', tickets => {
+        tickets.increments('ticket_id');
+        tickets.string('description', 128).notNullable();
+        tickets.string('status', 16).defaultTo('PENDING');
+        tickets.integer('created_by').references('user_id').inTable('users')
     })
 };
 
@@ -14,5 +27,7 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTableIfExists('users')
+  .dropTableIfExists('tickets');
 };
