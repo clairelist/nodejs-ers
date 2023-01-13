1bb5e50fc968ed717a0f959cd6c710c0e650aca0
clairelist <claire.list2501@gmail.com>
2023-01-13 11:39:00 -0800

    DAMN IT MIGRATIONS

---
+++ data/migrations/20230113192849_amount.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('tickets', function (tickets){
+        tickets.string('amount', 5);
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
