7afe3a1c9e0528fb4100672ee7e1f4d4db251416
clairelist <claire.list2501@gmail.com>
2023-01-18 12:14:31 -0800

    some notes

--- api/users/router.js
+++ api/users/router.js
@@ -70,7 +70,7 @@ router.post('/register', async (req, res, next)=>{
           const token = buildToken(req.body);
           res.status(200).json({message:`Welcome, ${user.first_name}.`, token});
         } else {
-          res.status(403).json({message: 'Bad credentials supplied.'}); //TODO: REFACTOR TO CALL NEXT HERE.
+          res.status(401).json({message: 'Bad credentials supplied.'}); //TODO: REFACTOR TO CALL NEXT HERE.
         }
 
       } catch (err) {
