866ad3b4e0f36c8105c2003fa5d3274ff563e8eb
clairelist <claire.list2501@gmail.com>
2023-01-23 20:13:44 -0800

    comit for monday

---
+++ api/tickets/middlewares.js
@@ -0,0 +1,6 @@
+//restricted, which does not allow access if req.headers.auth token is not supplied
+// --> view own tix, create ticket
+
+//checkRole, prevents non-managers from updating tickets --> updateTicket, viewByStatus ?
+
+//checkOwner, restricts access to updating own ticket --> updateTicket
--- api/users/middlewares.js
+++ api/users/middlewares.js
@@ -1,5 +1,14 @@
 //import user model stuff, for checking password below
 
+
+
+//checkLogin, checks login details provided by user are equal to details in DB
+
+//checkEmailValid, checks that email is unique in DB on registration. Seperate from below!!!
+
+//validate, validates information on registration (password is acceptable length, character reqs?, no field is blank or contains whitespaces --> chekc with command re: edge cases of names having spaces "von Tscharner", etc)
+
+
 function checkPassword(req){
     if (req.body.password !== passFromDB){
         next(err);
