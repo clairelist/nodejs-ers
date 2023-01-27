d26972d14434d3cb9a848eb8b6ac8015fddfc939
clairelist <claire.list2501@gmail.com>
2023-01-27 14:06:46 -0800

    login middleware 1 built

--- api/users/middlewares.js
+++ api/users/middlewares.js
@@ -1,5 +1,7 @@
 //import user model stuff, for checking password below
-
+const User = require('./model');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('./secret');
 
 
 //checkLogin, checks login details provided by user are equal to details in DB
@@ -9,11 +11,22 @@
 //validate, validates information on registration (password is acceptable length, character reqs?, no field is blank or contains whitespaces --> chekc with command re: edge cases of names having spaces "von Tscharner", etc)
 
 
-function checkPassword(req){
-    if (req.body.password !== passFromDB){
-        next(err);
-    } 
-    next(req.body);
+function checkPassword(req, res, next){
+
+    let token = req.headers.authorization;
+
+    if (!token){ //error checking...
+        res.status(401).json({message: "A TOKEN is REQUIRED."})
+    } else {
+        jwt.verify(token, JWT_SECRET, (error, decoded)=>{
+            if (error) {
+                res.status(401).json({message: "TOKEN is INVALID."})
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        })
+    }
 }
 
 module.exports = {
